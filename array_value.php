d514b1417df48e932519e12cfc8411b41877584a
Prog-Coder-devops <90681344+Prog-Coder-devops@users.noreply.github.com>
2024-08-06 10:51:08 +0530

    Update array_value.php
    change in  date

--- array_value.php
+++ array_value.php
@@ -1,6 +1,6 @@
 <?php 
 	$july_2024 = array("red","green");
-	$August_2024 = array("blue","yellow");
+	echo $August_2024 = array("blue","yellow");
 	$full_year = array_merge($july_2024,$August_2024);
 echo "ttt";
 ?>
